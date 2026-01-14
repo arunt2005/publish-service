31eb1f112964cfe035808e48a87844da414654c7
arunt2005 <arunt2005@gmail.com>
2026-01-15 03:49:46 +0530

    queue name config.

--- src/main/java/com/apps/config/JmsConfig.java
+++ src/main/java/com/apps/config/JmsConfig.java
@@ -1,9 +1,16 @@
 package com.apps.config;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 
 @Configuration
 @EnableJms
 public class JmsConfig {
+
+    @Bean
+    public Queue testQueue() {
+        return new Queue("test.queue", true);
+    }
 }
